f93ebedceba1bcbfaebc75181fccf655b3c905ec
lmissel <39304706+lmissel@users.noreply.github.com>
2019-08-02 23:59:28 +0200

    Update System.IO.MemoryMappedFiles.Commands.psm1

--- System.IO.MemoryMappedFiles.Commands.psm1
+++ System.IO.MemoryMappedFiles.Commands.psm1
@@ -1,3 +1,15 @@
+# -----------------------------------------------
+# Module: System.IO.MemoryMappedFiles.Commands
+# 
+# Dieses PowerShell Module stellt Funktionen für sogenannte Speicherabbilddateien zur Verfügung. Speicherabbilddateien ermöglichen Programmierern, mit sehr großen Dateien arbeiten zu können, da der Speicher gleichzeitig verwaltet werden kann. Speicherabbilddateien können auch von mehreren Prozessen verwendet werden, wodurch der Austausch von Daten, mit Hilfe von Speicherabbilddateien, über mehrere PowerShell Instanzen ermöglicht werden kann.
+# Ebenso ist das Einlesen von vorhandenen Dateien möglich, wodurch ein schnellerer Zugriff auf die Daten ermöglicht wird.
+#
+# ToDo-Liste:
+# - CreateFromFile implementation
+# - Get and Set AccessControl
+#
+# -----------------------------------------------
+
 function New-MemoryMappedFile
 {
     [CmdletBinding(DefaultParameterSetName='Default', 
